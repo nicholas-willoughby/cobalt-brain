d4b4975617ac81804d4e38637fce4985fca4e916
Nicholas Willoughby <will97echo@outlook.com>
2026-04-10 16:52:05 -0400

    fix router definition + XML SWML

--- app/routes/call.py
+++ app/routes/call.py
@@ -3,8 +3,17 @@ from fastapi.responses import Response
 
 router = APIRouter()
 
+@router.get("/call")
+async def handle_call_get():
+    xml = """
+    <Response>
+        <Say>Your FastAPI webhook is now connected.</Say>
+    </Response>
+    """
+    return Response(content=xml, media_type="application/xml")
+
 @router.post("/call")
-async def handle_call():
+async def handle_call_post():
     xml = """
     <Response>
         <Say>Your FastAPI webhook is now connected.</Say>
